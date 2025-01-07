ab2799f15c280805c6ba3d79047aa069b54d0f57
AdarshTrivedi123 <adarshtrivedi20022020@gmail.com>
2025-01-07 13:05:58 +0530

    Response format updated

--- rec_system/serializers.py
+++ rec_system/serializers.py
@@ -8,7 +8,12 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
-
+    
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with this username already exists.")
+        return value
+    
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError("A user with this email already exists.")
--- rec_system/views.py
+++ rec_system/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -11,24 +12,34 @@ from .serializers import RegisterSerializer, UserProfileDataSerializer
 class RegisterAPIView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, _ = Token.objects.get_or_create(user=user)
-            response_data = {
-                "success": True,
-                "message": "Logged In Successfully",
-                "data": {
-                    "Token": token.key,
-                    "user_details": {
+        try:
+            if serializer.is_valid(raise_exception=True):  
+                user = serializer.save()
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({
+                    "success": True,
+                    "message": "User registered successfully.",
+                    "data": {
+                        "Token": token.key,
                         "first_name": user.first_name,
                         "last_name": user.last_name,
                         "username": user.username,
-                    }
-                }
+                        "email": user.email,
+                    },
+                }, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            response_data = {
+                "success": False,
+                "message": None
             }
-            return Response(response_data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if 'email' in e.detail:
+                response_data["message"] = e.detail['email'][0]
+            elif 'username' in e.detail:
+                response_data["message"] = e.detail['username'][0]
+            else:
+                response_data["message"] = "Invalid data provided."
+            
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginAPIView(APIView):
@@ -38,9 +49,15 @@ class LoginAPIView(APIView):
         user=authenticate(username=username,password=password)
         if user:
             token, _=Token.objects.get_or_create(user=user)
-            return Response({"Token":token.key},status=status.HTTP_201_CREATED)
+            return Response({
+                    "success": True,
+                    "message": "User logged-in successfully.",
+                    "data": {
+                        "Token": token.key,
+                    },
+                }, status=status.HTTP_201_CREATED)
         
-        return Response({"error":"Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({"success":False, "message":"Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
     
 class UserProfileDataView(APIView):
     def post(self, request, username):
